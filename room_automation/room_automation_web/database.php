e9ada3a632dff10bfcf93632deb9428182e4dee9
Nav3011 <naveengondu18@gmail.com>
2018-10-16 13:18:04 +0530

    removed files

--- room_automation/room_automation_web/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysql_host = 'localhost';
-$mysql_user = 'root';
-$mysql_password = '';
-$mysql_dbname = 'room automation';
-$con = mysqli_connect($mysql_host , $mysql_user , $mysql_password , $mysql_dbname);
-?>
